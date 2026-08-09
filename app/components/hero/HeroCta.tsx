31e3f93b11f7bdb308dce177bd0af1940e704642
Abdulganiy Ibrahim <abdulganiyibrahim095@gmail.com>
2026-08-09 08:01:19 +0100

    FEAT: added my cv

--- app/components/hero/HeroCta.tsx
+++ app/components/hero/HeroCta.tsx
@@ -13,12 +13,14 @@ export default function HeroCta() {
         <ArrowUpRight className='h-4 w-4' />
       </Link>
 
-      <Button
+      <Link
+        href='/Abdulganiy-Ibrahim-CV.pdf'
+        download='Abdulganiy-Ibrahim-CV.pdf'
         className='inline-flex items-center gap-4 bg-background text-primary border border-primary rounded-2xl py-2 px-3 hover:bg-primary hover:text-white hover:shadow-emerald transition-all duration-300'
       >
         Download CV
         <Download className='h-4 w-4' />
-      </Button>
+      </Link>
     </div>
   )
 }
